154786f050b4e90ad8885da4eaf2ce80ea4078dd
AliaxeiSehiyenia <alexeisergienia@gmail.com>
2022-05-17 11:48:59 +0300

    feat: translate page signIn, replace spinner to loading

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,12 +1,13 @@
-import { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState, useCallback, Suspense } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import { AxiosError } from 'axios';
 import { useForm } from 'react-hook-form';
 import { Box, Grid, Paper, TextField, Typography, Button } from '@mui/material';
 import { loginUser } from '../../services/SignInUpService';
 import { config } from '../../config';
 import { useAppDispatch, useAppSelector } from '../../hooks/redux';
-import Spinner from '../../components/Spinner/Spinner';
+import { Loading } from '../../components/Loading';
 import { onChangeLogin, onChangePassword, clearCurrentUser } from './Reducer/FormSignInSlice';
 import '../../App.css';
 
@@ -16,6 +17,7 @@ interface IUserInfo {
 }
 
 export function SignIn() {
+  const { t } = useTranslation(['common', 'pages_registration']);
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const { login, password } = useAppSelector((state) => state.formSignInReducer.currentUser);
@@ -46,13 +48,13 @@ export function SignIn() {
 
   const renderValidationMessage = useCallback(
     (message: string | undefined) => (
-      <span className={'span-not-invalid'}>{message || 'this field is required'}</span>
+      <span className={'form-span-not-invalid'}>{message || 'this field is required'}</span>
     ),
     []
   );
 
   const renderUserSignInError = useCallback(
-    (userError: string) => <span className={'span-not-invalid'}>{userError}</span>,
+    (userError: string) => <span className={'form-span-not-invalid'}>{userError}</span>,
     []
   );
 
@@ -79,71 +81,75 @@ export function SignIn() {
   const btnModalSubmit = { marginBottom: '12px', height: '40px' };
   const inputModal = { marginBottom: '30px' };
 
-  return isLogin ? (
-    <Spinner />
-  ) : (
-    <Box
-      component="main"
-      sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', py: 5, px: 2 }}
-    >
-      <Grid className="login-modal">
-        <Paper elevation={24} className="form-container">
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <h2 className="form-title">Sign In</h2>
-            <div className="container-input-message">
-              <TextField
-                {...register('login', {
-                  required: true,
-                  pattern: { value: /^[A-Za-z]+$/i, message: 'only letters of the Latin alphabet' },
-                  minLength: { value: 3, message: 'min length 3 letters' },
-                })}
-                style={inputModal}
-                name="login"
-                value={login}
-                label="login"
-                placeholder="Enter login"
+  return (
+    <Suspense fallback={<Loading fullScreen />}>
+      <Box
+        component="main"
+        sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', py: 5, px: 2 }}
+      >
+        <Grid className="login-modal">
+          <Paper elevation={24} className="form-container">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <h2 className="form-title">{t('common:navbar.sign_in')}</h2>
+              <div className="form-container-input-message">
+                <TextField
+                  {...register('login', {
+                    required: true,
+                    pattern: {
+                      value: /^[A-Za-z]+$/i,
+                      message: t('pages_registration:messages.only_letters'),
+                    },
+                    minLength: { value: 3, message: t('pages_registration:messages.min_length3') },
+                  })}
+                  style={inputModal}
+                  name="login"
+                  value={login}
+                  label={t('pages_registration:user.login')}
+                  placeholder={t('pages_registration:placeholder.login')}
+                  fullWidth
+                  onChange={(e) => dispatch(onChangeLogin((e.target as HTMLInputElement).value))}
+                />
+                {errors.login && renderValidationMessage(errors.login?.message)}
+              </div>
+              <div className="form-container-input-message">
+                <TextField
+                  {...register('password', {
+                    required: true,
+                    pattern: {
+                      value: /^[0-9]+$/g,
+                      message: t('pages_registration:messages.only_numbers'),
+                    },
+                    minLength: { value: 3, message: t('pages_registration:messages.min_length3') },
+                  })}
+                  style={inputModal}
+                  name="password"
+                  value={password}
+                  label={t('pages_registration:user.password')}
+                  placeholder={t('pages_registration:placeholder.password')}
+                  type="password"
+                  fullWidth
+                  onChange={(e) => dispatch(onChangePassword((e.target as HTMLInputElement).value))}
+                />
+                {errors.password && renderValidationMessage(errors.password?.message)}
+                {userError && renderUserSignInError(userError)}
+              </div>
+              <Button
+                type="submit"
+                color="primary"
+                variant="contained"
+                style={btnModalSubmit}
                 fullWidth
-                required
-                onChange={(e) => dispatch(onChangeLogin((e.target as HTMLInputElement).value))}
-              />
-              {errors.login && renderValidationMessage(errors.login?.message)}
-            </div>
-            <div className="container-input-message">
-              <TextField
-                {...register('password', {
-                  required: true,
-                  pattern: { value: /^[0-9]+$/g, message: 'only numbers' },
-                  minLength: { value: 3, message: 'min length 3 numbers' },
-                })}
-                style={inputModal}
-                name="password"
-                value={password}
-                label="Password"
-                placeholder="Enter password"
-                type="password"
-                fullWidth
-                required
-                onChange={(e) => dispatch(onChangePassword((e.target as HTMLInputElement).value))}
-              />
-              {errors.password && renderValidationMessage(errors.password?.message)}
-              {userError && renderUserSignInError(userError)}
-            </div>
-            <Button
-              type="submit"
-              color="primary"
-              variant="contained"
-              style={btnModalSubmit}
-              fullWidth
-            >
-              Sign In
-            </Button>
-            <Typography>
-              Do you have an account?&nbsp;
-              <Link to={config.urls.public.signUp}>Sign Up</Link>
-            </Typography>
-          </form>
-        </Paper>
-      </Grid>
-    </Box>
+              >
+                {t('common:navbar.sign_in')}
+              </Button>
+              <Typography>
+                {t('pages_registration:account')}
+                <Link to={config.urls.public.signUp}>{t('common:navbar.sign_up')}</Link>
+              </Typography>
+            </form>
+          </Paper>
+        </Grid>
+      </Box>
+    </Suspense>
   );
 }
